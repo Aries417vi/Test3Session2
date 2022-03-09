b2ac72bd5ff4d2ee1cc54c8b39f725b30543e126
aries <94883201+Aries417vi@users.noreply.github.com>
2022-03-09 09:14:00 +0000

    p4final

--- p3final.c
+++ p3final.c
@@ -1,48 +1,36 @@
-/*
-Write a program find whether a given number is a prime number.
-int input_number();
-int is_prime(int n);
-void output(int n, int is_prime);
-*/
-
 #include <stdio.h>
-#include <math.h>
-int input()
+#include<math.h>
+int fact(int);
+int input(int *n,int *r)
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+  printf("Enter a number:\n");
+  scanf("%d",n);
+  printf("Enter a number:\n");
+  scanf("%d",r);
 }
-
-int isprime(int n)
+int fact(int n)
 {
-  if (n==0 || n==1) {
-    return 0
-  }
-  if (n==2) {
-    return 1;
-  }
-  for(int i= 2; i<= sqrt(n);i++)
-  {
-    if(n%i==0)
-      return 0;
-  }
-  return 1;
+  int i,f=1;
+  for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    return f;
 }
-void output(int n, int isp)
+int ncr(int n,int r,int *result)
 {
-  if(isp == 0)
-    printf("%d is not a prime number\n",n);
-  else
-    printf("%d is a prime number\n",n)
+  *result=fact(n)/(fact(r)*fact(n-r));
+  return *result;
 }
-
+void output(int n,int r,int result)
+{
+  printf("Value of %dC%d = %d",n,r,result);
+}
 int main()
 {
-  int n = input();
-  int isp = isprime(n);
-  output(n,isp);
+  int n,r,result;
+  input(&n,&r);
+  ncr(n,r,&result);
+  output(n,r,result);
   return 0;
-  }
 }
